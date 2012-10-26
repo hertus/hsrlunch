801378930ee24bdca4781e007c6247cd87adff9c
Susanne Mathys <susi.mathys@gmail.com>
2012-10-26 14:01:19 +0200

    test

--- src/ch/hsr/hsrlunch/MainActivity.java
+++ src/ch/hsr/hsrlunch/MainActivity.java
@@ -1,6 +1,9 @@
 package ch.hsr.hsrlunch;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import android.os.Bundle;
@@ -8,7 +11,9 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.widget.Toast;
 
+import ch.hsr.hsrlunch.controller.OfferConstants;
 import ch.hsr.hsrlunch.model.Offer;
+import ch.hsr.hsrlunch.model.WorkDay;
 import ch.hsr.hsrlunch.util.TabPageAdapter;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
@@ -20,8 +25,15 @@ import com.viewpagerindicator.TabPageIndicator;
 
 public class MainActivity extends SherlockFragmentActivity implements OnSlideMenuItemClickListener {
 	
-    public static List<Offer> offerList;
+	public final static String OFFER_DAILY_TITLE = "Tagesteller";
+	public final static String OFFER_VEGI_TITLE = "Vegetarisch";
+	public final static String OFFER_WEEK_TITLE = "Wochen-Hit";
+	
+    private List<Offer> offerList;
+    public static List<WorkDay> dayList;
     public static List<String> tabTitleList;
+    
+    public static WorkDay selectedDay;
 	
     ViewPager mViewPager;
     TabPageAdapter mAdapter;
@@ -33,7 +45,7 @@ public class MainActivity extends SherlockFragmentActivity implements OnSlideMen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);       
         	
         init();
         
@@ -47,6 +59,7 @@ public class MainActivity extends SherlockFragmentActivity implements OnSlideMen
 		slidemenu = (SlideMenu) findViewById(R.id.slideMenu);
 		slidemenu.init(this, R.menu.slide, this, 333);
 		
+		
 //		slidemenu.setAsShown(); 		
 //		slidemenu.setHeaderImage(getResources().getDrawable(R.drawable.hsrlunch));
     }
@@ -56,18 +69,23 @@ public class MainActivity extends SherlockFragmentActivity implements OnSlideMen
 		switch(itemId) {
 		case R.id.item_one:
 			Toast.makeText(this, "Montag markiert", Toast.LENGTH_SHORT).show();
+			selectedDay = dayList.get(0);
 			break;
 		case R.id.item_two:
 			Toast.makeText(this, "Dienstag markiert", Toast.LENGTH_SHORT).show();
+			selectedDay = dayList.get(1);
 			break;
 		case R.id.item_three:
 			Toast.makeText(this, "Mittwoch markiert", Toast.LENGTH_SHORT).show();
+			selectedDay = dayList.get(2);
 			break;
 		case R.id.item_four:
 			Toast.makeText(this, "Donnerstag markiert", Toast.LENGTH_SHORT).show();
+			selectedDay = dayList.get(3);
 			break;
 		case R.id.item_five:
 			Toast.makeText(this, "Freitag markiert", Toast.LENGTH_SHORT).show();
+			selectedDay = dayList.get(4);
 			break;
 		}
 	}
@@ -83,21 +101,36 @@ public class MainActivity extends SherlockFragmentActivity implements OnSlideMen
 		return super.onOptionsItemSelected(item);
 	}
 	private void init() {
-		Offer m1 = new Offer("Tagesmenü",
-        		"Fischtäbli\nSauce Tatar\nBlattspinat\nSalzkartoffeln", 8.00, "Montag 1. Oktober");
-        Offer m2 = new Offer("Vegimenü",
-        		"Gemüseteigtaschen\nTomatensauce\nSalzkartoffeln\nBuntersalat", 8.00, "Montag 1. Oktober");
-        Offer m3 = new Offer( "Wochenhit",
-        		"Schweinefilet im Speckmantel\nTomatensauce\nBuntersalat", 14.50, "Montag 1. Oktober");
+		
+		Offer m1 = new Offer(0,	"Fischtäbli\nSauce Tatar\nBlattspinat\nSalzkartoffeln", "INT 8.00 EXT 10.60");
+		Offer m2 = new Offer(1, "Gemüseteigtaschen\nTomatensauce\nSalzkartoffeln\nBuntersalat", "INT 8.00 EXT 10.60");
+		Offer m3 = new Offer(2, "Schweinefilet im Speckmantel\nTomatensauce\nBuntersalat", "INT 14.50 EXT 15.50");
         offerList = new ArrayList<Offer>();
         offerList.add(m1);
         offerList.add(m2);
         offerList.add(m3);
         
+        GregorianCalendar cal1 = new GregorianCalendar(2012, 10,22);
+        GregorianCalendar cal2 = new GregorianCalendar(2012, 10,23);
+        GregorianCalendar cal3 = new GregorianCalendar(2012, 10,24);
+        GregorianCalendar cal4 = new GregorianCalendar(2012, 10,25);
+        GregorianCalendar cal5 = new GregorianCalendar(2012, 10,26);
+        
+		dayList = new ArrayList<WorkDay>();
+		dayList.add(new WorkDay(0,new Date(cal1.getTimeInMillis()), offerList));
+		dayList.add(new WorkDay(1,new Date(cal2.getTimeInMillis()), offerList));
+		dayList.add(new WorkDay(2,new Date(cal3.getTimeInMillis()), offerList));
+		dayList.add(new WorkDay(3,new Date(cal4.getTimeInMillis()), offerList));
+		dayList.add(new WorkDay(4,new Date(cal5.getTimeInMillis()), offerList));
+		
         tabTitleList = new ArrayList<String>(3);
         tabTitleList.add("tages");
         tabTitleList.add("vegi");
         tabTitleList.add("woche");
+        
+        GregorianCalendar cal = new GregorianCalendar();   
+        System.out.println("dayselection: "+dayList.get( (cal.get(Calendar.DAY_OF_WEEK)+5) % 7));
+        selectedDay = dayList.get(0);
 	} 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
