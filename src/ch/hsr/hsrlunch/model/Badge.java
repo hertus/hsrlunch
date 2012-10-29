5efd93573a8121841ee0880101ee65042b7d9067
Susanne Mathys <susi.mathys@gmail.com>
2012-10-30 00:39:18 +0100

    changed Date format & new Model class Badge

---
+++ src/ch/hsr/hsrlunch/model/Badge.java
@@ -0,0 +1,31 @@
+package ch.hsr.hsrlunch.model;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class Badge {
+	double amount;
+	Date lastUpdate;
+	DateFormat df;
+	
+	public Badge(double amount, Date lastUpdate) {
+		super();
+		this.amount = amount;
+		this.lastUpdate = lastUpdate;
+		df = DateFormat.getDateInstance( DateFormat.FULL );
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public String getLastUpdate() {
+		return df.format(lastUpdate);
+	}
+	public void setLastUpdate(Date lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+	
+
+}
--- src/ch/hsr/hsrlunch/model/WorkDay.java
+++ src/ch/hsr/hsrlunch/model/WorkDay.java
@@ -1,24 +1,29 @@
 package ch.hsr.hsrlunch.model;
 
+import java.text.DateFormat;
 import java.util.Date;
 import java.util.List;
+
 
 public class WorkDay {
 
 	int id;
-
-
 	Date date;
 	List<Offer> offerList;
+	DateFormat df;
+
 
 	public WorkDay(int id, Date date, List<Offer> offerList) {
 		super();
 		this.id = id;
 		this.date = date;
 		this.offerList = offerList;
+		
+		df = DateFormat.getDateInstance( DateFormat.FULL );
+		   
 	}
-	public Date getDate() {
-		return date;
+	public String getDate() {
+		return df.format(date);
 	}
 	public List<Offer> getOfferList() {
 		return offerList;
