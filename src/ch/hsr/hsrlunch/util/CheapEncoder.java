246145eb3b91b57729d9e9f06f4dacb8f9091326
Hert <Hert@Hertus>
2012-12-13 22:21:16 +0100

    funktioniert nicht

--- src/ch/hsr/hsrlunch/util/CheapEncoder.java
+++ src/ch/hsr/hsrlunch/util/CheapEncoder.java
@@ -1,7 +1,6 @@
 package ch.hsr.hsrlunch.util;
 
 public class CheapEncoder {
-	private static final String WHATEVER = "LuQmksn09978$1M+hs65asf";
 
 	/**
 	 * 
@@ -9,21 +8,8 @@ public class CheapEncoder {
 	 * @return encrypted String
 	 */
 	public static String cheapEncode(CharSequence inputString) {
-		StringBuilder output = new StringBuilder();
-		inputString = inputString + WHATEVER;
-		for (int i = 0; i < inputString.length(); i++) {
-			char c = inputString.charAt(i);
-			if ((c < 45) || (c > 123)) {
-				output.append(c);
-			} else {
-				c += 42;
-				if (c > 123) {
-					c -= ((123 - 45) + 1);
-				}
-				output.append(c);
-			}
-		}
-		return output.toString();
+
+		return "";
 	}
 
 	/**
@@ -33,28 +19,7 @@ public class CheapEncoder {
 	 * @return decrypted String from simple encrypted String
 	 */
 	public static String cheapDecode(CharSequence inputString) {
-		if (inputString.length() <= WHATEVER.length()) {
-			// somethings wrong - can't be an encrypted input
-			return inputString.toString();
-		}
-		StringBuilder output = new StringBuilder();
-		for (int i = 0; i < inputString.length(); i++) {
-			char c = inputString.charAt(i);
-			if ((c < 45) || (c > 123)) {
-				output.append(c);
-			} else {
-				c -= 42;
-				if (c < 45) {
-					c += ((123 - 45) + 1);
-				}
-				output.append(c);
-			}
-		}
-		try {
-			output = new StringBuilder(output.subSequence(0, output.length()
-					- WHATEVER.length()));
-		} catch (StringIndexOutOfBoundsException e) {
-		}
-		return output.toString();
+		
+		return "";
 	}
 }
